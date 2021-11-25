297a5973fc25517838d6e10c0855688ed8e8242f
Skivman <al.sitkevich@gmail.com>
2021-11-25 22:12:23 +0300

    1.9. Время разбивать камни

--- src/loading-view.js
+++
@@ -1,8 +0,0 @@
-export const getLoadingMarkup = () => (
-  `<section class="films">
-  <section class="films-list">
-    <h2 class="films-list__title">Loading...</h2>
-  </section>
-</section>
-</main>`
-);

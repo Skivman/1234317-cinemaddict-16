f3dcf892b66f1f075639f45e26371681b2317eae
Skivman <al.sitkevich@gmail.com>
2021-12-05 13:55:01 +0300

    Шаблонизация

--- src/main.js
+++ src/main.js
@@ -15,35 +15,48 @@ import { renderMockComment } from './view/popup-comment-view.js';
 
 //Массив моковых карточек с описанием фильмов
 const mockCards = Array.from({length: 15}, renderMockFilmCard);
+const MAX_CARDS = 5;
 
-//Общая функция для отрисовки карточек на странице и добавлению попапа
- const renderFilmsInContainer = (cards) => {
-    const MAX_CARDS = 5;
- renderDomElement(siteMainElement, getListTemplate(), renderPosition.BEFOREEND);
- const mockFilmCardContainer = document.querySelector('.films-list__container');
- for (let i = 0; i < MAX_CARDS; i++) {
-   renderDomElement(mockFilmCardContainer, renderFilmCard(cards[i]), renderPosition.BEFOREEND);
- };
- };
+
+
+//Общая функция для отрисовки карточек на странице и добавлению попапа, реализация кнопки "Показать больше"
+const renderFilmsInContainer = (cards) => {
+  renderDomElement(siteMainElement, getListTemplate(), renderPosition.BEFOREEND);
+  const mockFilmCardContainer = document.querySelector('.films-list__container');
+  const showMoreButton = document.querySelector('.films-list__show-more');
+  for (let i = 0; i < MAX_CARDS; i++) {
+    renderDomElement(mockFilmCardContainer, renderFilmCard(cards[i]), renderPosition.BEFOREEND);
+  }
+  //Логика кнопки 'Show more'
+  let offset = MAX_CARDS;
+  showMoreButton.addEventListener('click', () => {
+    const nextFive = cards.slice(offset, offset + MAX_CARDS);
+    if (nextFive.length === 0) {
+      showMoreButton.classList.add('visually-hidden');
+    }
+    nextFive.forEach((card) => {
+      renderDomElement(mockFilmCardContainer, renderFilmCard(card), renderPosition.BEFOREEND);
+    });
+    offset += MAX_CARDS;
+  });
+};
 //Функция отрисовки попапа
- const renderPopup = (cards) => {
-   renderDomElement(siteMainElement, renderPopupTemplate(renderPopupMockData(cards[0])), renderPosition.BEFOREEND);
-   const popup = document.querySelector('.film-details');
-   const popupCloseButton = document.querySelector('.film-details__close-btn');
-   const commentsList = document.querySelector('.film-details__comments-list');
-   const randomComment = renderMockComment(generateComment())
+const renderPopup = (cards) => {
+  renderDomElement(siteMainElement, renderPopupTemplate(renderPopupMockData(cards[0])), renderPosition.BEFOREEND);
+  const popup = document.querySelector('.film-details');
+  const popupCloseButton = document.querySelector('.film-details__close-btn');
+  const commentsList = document.querySelector('.film-details__comments-list');
+  const randomComment = renderMockComment(generateComment());
   renderDomElement(commentsList, randomComment, renderPosition.BEFOREEND);
-   popupCloseButton.addEventListener('click', () => {
+  popupCloseButton.addEventListener('click', () => {
+    popup.classList.add('visually-hidden');
+  });
+  document.addEventListener('keydown', (evt) => {
+    if (evt.key === 'Escape') {
       popup.classList.add('visually-hidden');
-   })
-   document.addEventListener('keydown', (evt) => {
-      if (evt.key === 'Escape') {
-         popup.classList.add('visually-hidden');
-      }
-   })
- };
+    }
+  });
+};
 
- renderFilmsInContainer(mockCards);
- renderPopup(mockCards);
- showExtra(mockCards);
- 
+renderFilmsInContainer(mockCards);
+renderPopup(mockCards);
